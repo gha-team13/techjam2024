e419582aa44c2085d4f1e16c03d01a848cb4b49b
tech jam <adcost68@gmail.com>
2024-11-20 19:50:09 -0500

    Initial commit

---
+++ __tests__/page.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import Home from "../src/app/page";
+import "@testing-library/jest-dom";
+
+jest.mock("../src/utils/flagsmith", () => ({
+  getEnvironmentFlags: jest.fn(() =>
+    Promise.resolve({
+      isFeatureEnabled: () => true,
+    })
+  ),
+}));
+
+describe("Home", () => {
+  it("renders a heading", async () => {
+    render(await Home());
+
+    const docH = screen.getByRole("heading", {
+      name: "Hello World",
+      level: 2,
+    });
+
+    expect(docH).toBeInTheDocument();
+  });
+});
